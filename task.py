8142b7f5adaa80c286b98c3b273bda0ee2c21609
Tashle534 <tashy6703@gmail.com>
2024-11-29 14:37:21 +0000

    re

--- task.py
+++ task.py
@@ -3,7 +3,7 @@ def ages():
     username = input("Please enter your name")
     age = int(input("Enter your age").strip())
     age_valid = True
-
+    
     if  age <= 18 :
         return True
     return False
